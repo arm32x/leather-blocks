f430a4295562db82d887083ec8050443cdd96c62
arm32x <arm32.x@gmail.com>
2021-05-19 10:41:08 -0400

    Mode some initialization to the client.

--- src/main/java/arm32x/minecraft/leatherblocks/LeatherBlocks.java
+++ src/main/java/arm32x/minecraft/leatherblocks/LeatherBlocks.java
@@ -4,7 +4,6 @@ import arm32x.minecraft.leatherblocks.block.LeatherBlock;
 import arm32x.minecraft.leatherblocks.block.LeatherBlockEntity;
 import arm32x.minecraft.leatherblocks.item.LeatherBlockItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -21,8 +20,5 @@ public final class LeatherBlocks implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("leatherblocks", "leather_block"), LEATHER_BLOCK_ITEM);
 
 		Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier("leatherblocks", "leather_block"), LEATHER_BLOCK_ENTITY);
-
-		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> LeatherBlockEntity.getColor(world, pos), LEATHER_BLOCK);
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> LEATHER_BLOCK_ITEM.getColor(stack), LEATHER_BLOCK_ITEM);
 	}
 }
--- src/main/java/arm32x/minecraft/leatherblocks/client/LeatherBlocksClient.java
+++ src/main/java/arm32x/minecraft/leatherblocks/client/LeatherBlocksClient.java
@@ -1,13 +1,17 @@
 package arm32x.minecraft.leatherblocks.client;
 
+import arm32x.minecraft.leatherblocks.LeatherBlocks;
+import arm32x.minecraft.leatherblocks.block.LeatherBlockEntity;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 
 @Environment(EnvType.CLIENT)
 public final class LeatherBlocksClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-
+		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> LeatherBlockEntity.getColor(world, pos), LeatherBlocks.LEATHER_BLOCK);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> LeatherBlocks.LEATHER_BLOCK_ITEM.getColor(stack), LeatherBlocks.LEATHER_BLOCK_ITEM);
 	}
 }
